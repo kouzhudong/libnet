d85f9ef51c262f69eb59929e39ec8a5127592347
correy <112426112@qq.com>
2021-11-22 11:50:31 +0800

    改进。

--- libnet/raw.cpp
+++ libnet/raw.cpp
@@ -9,7 +9,7 @@ void InitEthernet4Header(IN PBYTE SrcMac, OUT PRAW_TCP buffer)
 /*
 功能：填写IPv4用的以太头。
 
-注释：因为packet的data是IPv4头，这里没有MAC，所以这里填写虚假的目的MAC，但也可发送出去。
+注释：填写虚假的目的MAC，也可发送出去。
 */
 {
     buffer->eth_hdr.Destination.Byte[0] = 0x99;
@@ -30,15 +30,16 @@ void InitEthernet4Header(IN PBYTE SrcMac, OUT PRAW_TCP buffer)
 }
 
 
-void InitIpv4HeaderForTcp(PIPV4_HEADER in_ipv4, OUT PRAW_TCP buffer)
+void InitIpv4Header(IN PIN_ADDR SourceAddress,
+                    IN PIN_ADDR DestinationAddress,
+                    IN UINT8 Protocol, //取值，如：IPPROTO_TCP等。
+                    OUT PRAW_TCP buffer
+)
 /*
-功能：把packet的SYN包里的ipv4信息组装为buffer的要发生的ACK的ipv4。
+功能：组装IPv4头。
 */
 {
     ASSERT(buffer);
-
-    BYTE version = in_ipv4->Version;
-    ASSERT(IPV4_VERSION == version);
 
 #pragma warning( push )
 #pragma warning( disable : 4366 ) //一元“&”运算符的结果可能是未对齐的
@@ -51,16 +52,26 @@ void InitIpv4HeaderForTcp(PIPV4_HEADER in_ipv4, OUT PRAW_TCP buffer)
                                   sizeof(TCP_OPT)); //还有内容，可以再加别的。
     out_ipv4->DontFragment = TRUE;
     out_ipv4->TimeToLive = 128;
-    out_ipv4->Protocol = IPPROTO_TCP;
-    out_ipv4->SourceAddress.S_un.S_addr = in_ipv4->DestinationAddress.S_un.S_addr;
-    out_ipv4->DestinationAddress.S_un.S_addr = in_ipv4->SourceAddress.S_un.S_addr;
+    out_ipv4->Protocol = Protocol;// IPPROTO_TCP;
+    out_ipv4->SourceAddress.S_un.S_addr = SourceAddress->S_un.S_addr; 
+    out_ipv4->DestinationAddress.S_un.S_addr = DestinationAddress->S_un.S_addr;
     out_ipv4->HeaderChecksum = checksum((unsigned short *)out_ipv4, sizeof(IPV4_HEADER));
 }
 
 
-void InitTcp4Header(PIPV4_HEADER ipv4, OUT PRAW_TCP buffer)
+void InitIpv4Header(IN PIPV4_HEADER in_ipv4, OUT PRAW_TCP buffer)
+/*
+功能：把packet的SYN包里的ipv4信息组装为buffer的要发生的ACK的ipv4。
+
+注意：如果是回复的包，要把源和目的换一下。
+*/
 {
-    BYTE version = ipv4->Version;
+    InitIpv4Header(&in_ipv4->SourceAddress, &in_ipv4->DestinationAddress, in_ipv4->Protocol, buffer);
+}
+
+
+void InitTcp4HeaderWithAck(PIPV4_HEADER ipv4, OUT PRAW_TCP buffer)
+{
     PVOID NextHeader = ((PBYTE)ipv4 + Ip4HeaderLengthInBytes(ipv4));
 
     PTCP_HDR tcp = (PTCP_HDR)NextHeader;
@@ -69,7 +80,6 @@ void InitTcp4Header(PIPV4_HEADER ipv4, OUT PRAW_TCP buffer)
 
     ASSERT(buffer);
     ASSERT(NextHeader);
-    ASSERT(IPV4_VERSION == version);
     ASSERT(TH_SYN == tcp->th_flags);
 
     LONG th_ack = ntohl(tcp->th_seq);
@@ -155,8 +165,8 @@ void PacketizeAck4(IN PIPV4_HEADER in_ipv4, IN PBYTE SrcMac, OUT PRAW_TCP buffer
     ASSERT(buffer);
 
     InitEthernet4Header(SrcMac, buffer);
-    InitIpv4HeaderForTcp(in_ipv4, buffer);
-    InitTcp4Header(in_ipv4, buffer);
+    InitIpv4Header(in_ipv4, buffer);
+    InitTcp4HeaderWithAck(in_ipv4, buffer);
 
     InitTcpMss(buffer);
     InitTcpWs(buffer);
@@ -173,7 +183,7 @@ void InitEthernet6Header(IN PBYTE SrcMac, OUT PRAW6_TCP buffer)
 /*
 功能：填写IPv6用的以太头。
 
-注释：因为packet的data是IPv6头，这里没有MAC，所以这里填写虚假的目的MAC，但也可发送出去。
+注释：填写虚假的目的MAC，也可发送出去。
 */
 {
     buffer->eth_hdr.Destination.Byte[0] = 0x99;
@@ -194,27 +204,42 @@ void InitEthernet6Header(IN PBYTE SrcMac, OUT PRAW6_TCP buffer)
 }
 
 
-void InitIpv6HeaderForTcp(PIPV6_HEADER in_ipv6, OUT PRAW6_TCP buffer)
+void InitIpv6Header(IN PIN6_ADDR SourceAddress,
+                    IN PIN6_ADDR DestinationAddress,
+                    IN UINT8 NextHeader, //取值，如：IPPROTO_TCP等。
+                    OUT PRAW6_TCP buffer
+)
 /*
-功能：把packet的SYN包里的ipv6信息组装为buffer的要发生的ACK的ipv6。
+功能：组装IPv6协议的TCP头。
 */
 {
     ASSERT(buffer);
 
-#pragma warning(suppress:4366) //一元“&”运算符的结果可能是未对齐的
+//#pragma warning(suppress:4366) //一元“&”运算符的结果可能是未对齐的
     PIPV6_HEADER ipv6_hdr = (PIPV6_HEADER)&buffer->ip_hdr;
 
     ipv6_hdr->VersionClassFlow = ntohl((6 << 28) | (0 << 20) | 0);// IPv6 version (4 bits), Traffic class (8 bits), Flow label (20 bits)
     ipv6_hdr->PayloadLength = ntohs(sizeof(TCP_HDR) + sizeof(TCP_OPT));
-    ipv6_hdr->NextHeader = IPPROTO_TCP;
+    ipv6_hdr->NextHeader = NextHeader;
     ipv6_hdr->HopLimit = 128;
 
-    RtlCopyMemory(&ipv6_hdr->SourceAddress, &in_ipv6->DestinationAddress, sizeof(IN6_ADDR));
-    RtlCopyMemory(&ipv6_hdr->DestinationAddress, &in_ipv6->SourceAddress, sizeof(IN6_ADDR));
+    RtlCopyMemory(&ipv6_hdr->SourceAddress, SourceAddress, sizeof(IN6_ADDR));
+    RtlCopyMemory(&ipv6_hdr->DestinationAddress, DestinationAddress, sizeof(IN6_ADDR));
 }
 
 
-void InitTcp6Header(PIPV6_HEADER ipv6, OUT PRAW6_TCP buffer)
+void InitIpv6Header(IN PIPV6_HEADER in_ipv6, OUT PRAW6_TCP buffer)
+/*
+功能：把packet的SYN包里的ipv6信息组装为buffer的要发生的ACK的ipv6。
+
+注意：如果是回复的包，要把源和目的换一下。
+*/
+{
+    InitIpv6Header(&in_ipv6->SourceAddress, &in_ipv6->DestinationAddress, in_ipv6->NextHeader, buffer);
+}
+
+
+void InitTcp6HeaderWithAck(PIPV6_HEADER ipv6, OUT PRAW6_TCP buffer)
 //和ipv4差不多。
 {
     ASSERT(buffer);
@@ -311,8 +336,8 @@ void PacketizeAck6(IN PIPV6_HEADER ipv6, IN PBYTE SrcMac, OUT PRAW6_TCP buffer)
     ASSERT(buffer);
 
     InitEthernet6Header(SrcMac, buffer);
-    InitIpv6HeaderForTcp(ipv6, buffer);
-    InitTcp6Header(ipv6, buffer);
+    InitIpv6Header(ipv6, buffer);
+    InitTcp6HeaderWithAck(ipv6, buffer);
 
     InitTcp6Mss(buffer);
     InitTcp6Ws(buffer);
