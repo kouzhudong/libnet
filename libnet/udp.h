33f5a053ce1e0e5c87c84198c58eaeda24b043b7
correy <112426112@qq.com>
2023-03-14 18:18:23 +0800

    改进。

--- libnet/udp.h
+++ libnet/udp.h
@@ -6,6 +6,18 @@
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
 
+//Õª×Ô£ºWindows-classic-samples\Samples\Win7Samples\netds\winsock\iphdrinc\iphdr.h
+typedef struct udp_hdr {// Define the UDP header 
+    unsigned short src_portno;       // Source port no.
+    unsigned short dst_portno;       // Dest. port no.
+    unsigned short udp_length;       // Udp packet length
+    unsigned short udp_checksum;     // Udp checksum
+} UDP_HDR, * PUDP_HDR;
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 EXTERN_C_START
 
 
