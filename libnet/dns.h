13145043fbe38bd4032fc0616b1d0d592bb58ded
correy <112426112@qq.com>
2023-04-19 09:55:01 +0800

    打印一些DNS信息。

--- libnet/dns.h
+++ libnet/dns.h
@@ -15,6 +15,9 @@ void WINAPI DnsQueryTest(int argc, char * argv[]);
 __declspec(dllexport)
 void WINAPI ModifyRecords(int argc, wchar_t * argv[]);
 
+__declspec(dllexport)
+VOID WINAPI PrintDnsRecordList(PDNS_RECORD DnsRecord);
+
 
 EXTERN_C_END
 
