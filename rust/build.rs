34daf4cc264596a0b896ae9fa3410f2454d78cc5
correy <112426112@qq.com>
2023-06-26 23:25:58 +0800

    改进。

--- rust/build.rs
+++ rust/build.rs
@@ -16,6 +16,11 @@ fn main() {
         .write_to_file(out_path.join("bindings.rs"))//"../inc/libnet.rs" 好家伙生成的文件达20多MB，66万行之多，运行也得几分钟。
         .expect("Couldn't write bindings!");
 
+        //相当于：#[link(name = "..\\x64\\Debug\\libnet")]，此办法经测试有效。
+        println!("cargo:rustc-link-search=native={}", "..\\x64\\Debug");
+        println!("cargo:rustc-link-lib=dylib=libnet");
+        println!("cargo:rerun-if-changed=../inc/libnet.h");
+
         // cxx_build::bridge("src/main.rs")  // returns a cc::Build
         // .file("../inc/libnet.h")
         // .flag_if_supported("-std=c++11")
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,8 @@
+/*
+如果由c转的rs文件在编译是出现：error[E0588]: packed type cannot transitively contain a `#[repr(align)]` type
+直接注释掉#[repr(align)]这一行即可，但是相关的测试可能错误：内存对齐问题。
+*/
+
 // Suppress the flurry of warnings caused by using "C" naming conventions
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
@@ -11,12 +16,12 @@
 // 	//可以为空。
 // }
 
-// include!(concat!(env!("OUT_DIR"), "\\bindings.rs"));
+include!(concat!(env!("OUT_DIR"), "\\bindings.rs"));
 
 fn main() {//测试的时候，要把dll复制到exe的目录下。
     println!("Hello, world!");
 
-    // unsafe{
-    //     EnumTcpTable();
-    // }
+    unsafe{
+        EnumTcpTable();
+    }
 }
