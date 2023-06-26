e6743578aa0e2a02853918e56c63c399ceb828c2
correy <112426112@qq.com>
2023-06-26 15:10:08 +0800

    添加自动生成rs文件的rust代码。

---
+++ rust/build.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // Configure and generate bindings.
+    let bindings = bindgen::builder()
+        .header("../inc/libnet.h")
+        //.clang_arg("-x").clang_arg("c++")
+        .generate()
+        .expect("Couldn't write bindings!");
+
+    // Write the generated bindings to an output file.
+    bindings
+        .write_to_file("../inc/libnet.rs")//好家伙生成的文件达20多MB，66万行之多，运行也得几分钟。
+        .expect("Couldn't write bindings!");
+
+        // cxx_build::bridge("src/main.rs")  // returns a cc::Build
+        // .file("../inc/libnet.h")
+        // .flag_if_supported("-std=c++11")
+        // .compile("cxxbridge-demo");
+}
