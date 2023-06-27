596a8c060ea5ba8a794dcf8bebafeea942ce0a74
correy <112426112@qq.com>
2023-06-27 13:35:11 +0800

    改进，可以直接编译。

--- rust/build.rs
+++ rust/build.rs
@@ -4,6 +4,33 @@ use std::path::PathBuf;
 fn main() {
     // Configure and generate bindings.
     let bindings = bindgen::builder()
+        .use_core()
+
+        //.blocklist_type("embedded_panic_header") //https://github.com/MozhuCY/osx-sys/commit/4f7265b6757499355696e4e5d1a991ee9a26dbfb
+        //.blocklist_type("embedded_panic_header__bindgen_ty_1")
+
+        .blocklist_type("IMAGE_TLS_DIRECTORY") //https://github.com/Rust-SDL2/rust-sdl2/issues/1288
+        .blocklist_type("PIMAGE_TLS_DIRECTORY")
+        .blocklist_type("IMAGE_TLS_DIRECTORY64")
+        .blocklist_type("PIMAGE_TLS_DIRECTORY64")
+        .blocklist_type("_IMAGE_TLS_DIRECTORY64")
+
+        /*
+        如果由c转的rs文件在编译是出现：error[E0588]: packed type cannot transitively contain a `#[repr(align)]` type
+        直接注释掉#[repr(align)]这一行即可，但是相关的测试可能错误：内存对齐问题。
+        另一个思路是改为：#[repr(align(1))]
+
+        暂时去掉这两个功能。
+        */
+        .blocklist_type("raw_tcp")
+        .blocklist_type("RAW_TCP")
+        .blocklist_type("PRAW_TCP")
+        .blocklist_type("raw6_tcp")
+        .blocklist_type("RAW6_TCP")
+        .blocklist_type("PRAW6_TCP")
+        .blocklist_function("PacketizeAck4") //https://github.com/rust-lang/rust-bindgen/issues/1556
+        .blocklist_function("PacketizeAck6")
+
         .header("../inc/libnet.h")
         //.clang_arg("-x").clang_arg("c++")
         .generate()
