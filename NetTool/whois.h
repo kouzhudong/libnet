3ec6b5ed55a337e5b082ef5dc8621347cd92694d
correy <112426112@qq.com>
2024-02-01 09:29:56 +0800

    补遗。

---
+++ NetTool/whois.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//#include <sys/types.h>
+//#define WIN32_NO_STATUS
+//#include <windef.h>
+//#define _INC_WINDOWS
+//#include <winsock2.h>
+/* #include <sys/socket.h> */
+/* #include <netinet/in.h> */
+/* #include <netdb.h> */
+//#include <stdio.h>
+//#include <stdlib.h>
+#include "pch.h"
+/* #include <various.h> */
+/* #include <getopt.h> */
+/* #include <io.h> */
+
+int whois(int argc, char ** argv);
