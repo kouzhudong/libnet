015299cf08c1b0082b9eb2c70da16c485379df70
correy <112426112@qq.com>
2024-09-03 12:38:10 +0800

    test

--- test/test.cpp
+++ test/test.cpp
@@ -37,12 +37,12 @@ int _cdecl main(_In_ int argc, _In_reads_(argc) CHAR * argv[])
     }
 
     //EnumWfpInfo(Args, Arglist);//宽字符函数入口示例。
-    //tracert(argc, argv);//单字符函数入口示例。
+    getaddrinfo_test(argc, argv); //单字符函数入口示例。
 
     //test_c();
 
     //EnumTcp6Table2();
-    EnumExtendedTcpTable(AF_INET, TCP_TABLE_OWNER_MODULE_ALL);
+    //EnumExtendedTcpTable(AF_INET, TCP_TABLE_OWNER_MODULE_ALL);
     //TestNetworkListManagerEvents();
     //ListenToNetworkConnectivityChangesSample(false);
 
